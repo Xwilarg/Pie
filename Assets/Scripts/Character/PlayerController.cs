551d4195a3234891e14fdac314683f406e350a9d
Xwilarg <xwilarg@yahoo.fr>
2021-08-14 21:51:26 +0200

    Add player movements

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -6,15 +6,36 @@ namespace Pie
     public class PlayerController : MonoBehaviour
     {
         private Rigidbody2D _rb;
+        private SpriteRenderer _sr;
+        private float _movement;
 
         private void Start()
         {
             _rb = GetComponent<Rigidbody2D>();
+            _sr = GetComponent<SpriteRenderer>();
         }
 
         private void FixedUpdate()
         {
-            _rb.velocity = new Vector2(Input.GetAxis("Horizontal"), _rb.velocity.y);
+            _rb.velocity = new Vector2(_movement, _rb.velocity.y);
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _movement = context.ReadValue<Vector2>().x;
+            if (_movement < 0f)
+            {
+                _sr.flipX = true;
+            }
+            else if (_movement > 0f)
+            {
+                _sr.flipX = false;
+            }
+        }
+
+        public void OnJump(InputAction.CallbackContext _)
+        {
+            _rb.AddForce(Vector2.up, ForceMode2D.Impulse);
         }
     }
 }
