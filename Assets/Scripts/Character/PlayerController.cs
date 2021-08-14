574a95883c1bf06993120ceecf73a2446b1c1eef
Xwilarg <xwilarg@yahoo.fr>
2021-08-15 01:12:37 +0200

    Add dash

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -9,11 +9,20 @@ namespace Pie
         private Animator _anim;
         private float _movement;
 
+        // Jump information
         private const float _distanceWithGround = .1f; // Used for jump raycast
         private const float _jumpForce = 3.5f;
-
         private bool _jump;
         private bool _isGrounded;
+
+        // Dash information
+        private bool _wasGoingLeft;
+        private bool _isDashingLeft;
+        private float _dashTimer;
+        private const float _dashTimerRef = .5f; // Duration of the dash in seconds
+        private const float _dashSpeed = 3f;
+        private float _doubleTapDelay; // Delay between 2 press to double tap
+        private const float _doubleTapDelayRef = .5f; // Nb of seconds we have to double tap
 
         private void Start()
         {
@@ -23,7 +32,14 @@ namespace Pie
 
         private void FixedUpdate()
         {
-            _rb.velocity = new Vector2(_movement, _rb.velocity.y);
+            if (_dashTimer > 0f)
+            {
+                _rb.velocity = new Vector2(_dashSpeed * (_isDashingLeft ? -1f : 1f), _rb.velocity.y);
+            }
+            else
+            {
+                _rb.velocity = new Vector2(_movement, _rb.velocity.y);
+            }
             if (_jump)
             {
                 _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
@@ -41,9 +57,48 @@ namespace Pie
             }
         }
 
+        private void Update()
+        {
+            if (_dashTimer > 0f)
+            {
+                _dashTimer -= Time.deltaTime;
+                if (_dashTimer <= 0f) // We are not dashing anymore, remove dashing animation
+                {
+                    _anim.SetBool("IsDashing", false);
+                }
+            }
+            if (_doubleTapDelay > 0f)
+            {
+                _doubleTapDelay -= Time.deltaTime;
+            }
+        }
+
         public void OnMove(InputAction.CallbackContext context)
         {
             _movement = context.ReadValue<Vector2>().x;
+
+            // We only check for dash when we are pressing the button
+            if (context.action.phase == InputActionPhase.Started
+                && _dashTimer <= 0f) // Can't dash while we are already dashing
+            {
+                // Dash
+                if (_doubleTapDelay > 0f // Can we still double tap
+                    && ((_wasGoingLeft && _movement < 0f) || (!_wasGoingLeft && _movement > 0f))) // Are we going in same direction as previously
+                {
+                    _dashTimer = _dashTimerRef; // Dashing
+                    _doubleTapDelay = 0f; // Reset delay
+                    _isDashingLeft = _movement < 0f; // Set dashing direction (used for movements)
+                    _anim.SetBool("IsDashing", true);
+                }
+                else if (_movement != 0f)
+                {
+                    _doubleTapDelay = _doubleTapDelayRef; // Reset double tap delay since we pressed a key
+                    _wasGoingLeft = _movement < 0f; // Old direction we were going to
+                }
+            }
+
+
+            // Movement
             if (_movement < 0f)
             {
                 transform.localScale = new Vector3(-1f, 1f, 1f);
